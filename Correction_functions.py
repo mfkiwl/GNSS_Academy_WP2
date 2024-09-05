86d0a8876d19a4b484173b88d7d002e200941599
Hao Feng <fengchenfu1@gmail.com>
2024-09-05 15:04:10 +0200

    Implementing computeLeoComPos function

--- Correction_functions.py
+++ Correction_functions.py
@@ -1,6 +1,27 @@
-
-
-
+from InputOutput import LeoPosIdx, LeoQuatIdx, SatPosIdx, SatApoIdx, SatClkIdx, SatBiaIdx
 
 def computeLeoComPos(Sod, LeoPosInfo):
-    pass
+
+    xPos = yPos = zPos = None
+
+    try:
+        # First we ensure Sod index
+        sod_index = LeoPosInfo.get("SOD").index(Sod)
+
+        # Iterate the keys of a given dictionary to acquire x, y and z position
+        for key in LeoPosInfo.keys():
+
+            # In case of finding one of them, it takes a list (value associated to that key) and finds the correspondent value using sod_index
+            if "x" in key:
+                xPos = LeoPosInfo.get(key)[sod_index]
+
+            elif "y" in key:
+                yPos = LeoPosInfo.get(key)[sod_index]
+
+            elif "z" in key:
+                zPos = LeoPosInfo.get(key)[sod_index]
+
+    except:
+        pass
+
+    return (xPos, yPos, zPos)
